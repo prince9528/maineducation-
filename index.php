685bf14a2f9bd846cf6a0940f6268b29573cf5bd
Abhishek Singh <abeypartapsingh@gmail.com>
2019-10-17 20:02:38 +0530

    Add some information

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
  <head>
  </head>
  <body>
-   <h3> welcome to the portal of freeeducation </h3>
+   <h3> welcome to the portal of free </h3>
+<h5>this portal is for learning purpose</h5>
+
    <p> this portal provides free exerciese for class 10 or 12th student of each chapter , each chapter have a multiple exercise</p>
    <p> first you need to sign up and then you will direct go to home page where you can select many class > subject > chapter > exercise
    </p>
    <p> if you donot to signup you can use our default mobile no and password  (mob no = 999887770) and (password= admin999)
   <a href="signin.php" > signin</a> 
+<h4>Thankyou and revisit again </h5>
  </body>
 </html>
